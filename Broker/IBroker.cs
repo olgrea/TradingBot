d33d054bd392028ad28e0333f4057431134b26f4
olgrea <olgrea@gmail.com>
2022-08-28 19:52:04 -0400

    retrieval of contracts (stocks only for now)

--- Broker/IBroker.cs
+++ Broker/IBroker.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TradingBot.Broker.MarketData;
 
 namespace TradingBot.Broker
 {
     internal interface IBroker
     {
         void Connect();
+        void Disconnect();
+        Contract GetContract(string ticker);
+        void RequestBars(Contract contract, Action<Contract, Bar> callback);
+        void CancelBarsRequest(Contract contract);
     }
 }
