c9c6c2aaf2714307e57135bbc11e837520c8e9e9
olgrea <olgrea@gmail.com>
2022-08-28 21:59:15 -0400

    LowerBBStrategy implementation (WIP)

---
+++ Strategies/BollingerBands.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TradingBot.Broker.MarketData;
+using MathNet.Numerics.Statistics;
+using System.Linq;
+
+namespace TradingBot.Strategies
+{
+    public class BollingerBands
+    {
+        const int nbPeriods = 20;
+
+        public Decimal MovingAverage { get; private set; }
+        public Decimal UpperBB { get; private set; }
+        public Decimal LowerBB { get; private set; }
+
+        LinkedList<Bar> _bars = new LinkedList<Bar>();
+
+        public bool IsReady => _bars.Count == nbPeriods;
+
+        public void Update(Bar bar)
+        {
+            _bars.AddLast(bar);
+            if(_bars.Count > nbPeriods)
+                _bars.RemoveFirst();
+
+            Compute();
+        }
+
+        void Compute()
+        {
+            var barsTypicalPrice = _bars.Select(b => Convert.ToDouble( (b.Close + b.High + b.Low)/3  ));
+            
+            MovingAverage = Convert.ToDecimal(Statistics.Mean(barsTypicalPrice));
+            var sdev = Convert.ToDecimal(Statistics.StandardDeviation(barsTypicalPrice));
+
+            UpperBB = MovingAverage + 2 * sdev;
+            LowerBB = MovingAverage - 2 * sdev;
+        }
+    }
+}
---
+++ Strategies/LowerBBStrategy.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingBot.Broker;
+using TradingBot.Broker.MarketData;
+
+namespace TradingBot.Strategies
+{
+    public class LowerBBStrategy : IStrategy
+    {
+        IState _currentState = null;
+
+        public LowerBBStrategy(Contract contract)
+        {
+            Contract = contract;
+
+            States = new Dictionary<string, IState>()
+            {
+                { nameof(StartState), new StartState(this)},
+                { nameof(MonitoringState), new MonitoringState(this)},
+                { nameof(OversoldState), new OversoldState(this)},
+                { nameof(RisingState), new RisingState(this)},
+                { nameof(SubmitBuyOrderState), new SubmitBuyOrderState(this)},
+            };
+        }
+
+        public Contract Contract { get; private set; }
+
+        public bool Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+        {
+            _currentState = _currentState.Evaluate(bar, bidAsk, out order);
+            return order != null;
+        }
+
+        public BollingerBands BB = new BollingerBands();
+
+        public readonly Dictionary<string, IState> States;
+
+        class StartState : IState
+        {
+            LowerBBStrategy _strat;
+            public StartState(LowerBBStrategy strat)
+            {
+                _strat = strat;
+            }
+
+            public IState Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+            {
+                order = null;
+
+                _strat.BB.Update(bar);
+                if (!_strat.BB.IsReady)
+                    return this;
+                else
+                    return _strat.States[nameof(MonitoringState)];
+            }
+        }
+
+        class MonitoringState : IState
+        {
+            LowerBBStrategy _strat;
+            public MonitoringState(LowerBBStrategy strat)
+            {
+                _strat = strat;
+            }
+
+            public IState Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+            {
+                order = null;
+                _strat.BB.Update(bar);
+
+                if(bar.Low <= _strat.BB.LowerBB)
+                    return _strat.States[nameof(OversoldState)];
+                else
+                    return this;
+            }
+        }
+
+        class OversoldState : IState
+        {
+            LowerBBStrategy _strat;
+            public OversoldState(LowerBBStrategy strat)
+            {
+                _strat = strat;
+            }
+
+            public IState Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+            {
+                order = null;
+                _strat.BB.Update(bar);
+
+                if (bar.Low <= _strat.BB.LowerBB)
+                    return this;
+                else
+                    return _strat.States[nameof(RisingState)];
+            }
+        }
+
+        class RisingState : IState
+        {
+            LowerBBStrategy _strat;
+            public RisingState(LowerBBStrategy strat)
+            {
+                _strat = strat;
+            }
+
+            public IState Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+            {
+                order = null;
+                _strat.BB.Update(bar);
+
+                if (bar.Low <= _strat.BB.LowerBB)
+                    return _strat.States[nameof(OversoldState)]; 
+                else
+                    return _strat.States[nameof(SubmitBuyOrderState)];
+            }
+        }
+
+        class SubmitBuyOrderState : IState
+        {
+            LowerBBStrategy _strat;
+            public SubmitBuyOrderState(LowerBBStrategy strat)
+            {
+                _strat = strat;
+            }
+
+            public IState Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+            {
+                _strat.BB.Update(bar);
+                
+                
+                order = new Order();
+
+                //if (/*order is filled*/)
+                    return _strat.States[nameof(BoughtState)];
+                //else
+                //    return _strat.States[nameof(MonitoringState)];
+            }
+        }
+
+        class BoughtState : IState
+        {
+            LowerBBStrategy _strat;
+            public BoughtState(LowerBBStrategy strat)
+            {
+                _strat = strat;
+            }
+
+            public IState Evaluate(Bar bar, BidAsk bidAsk, out Order order)
+            {
+                _strat.BB.Update(bar);
+                order = null;
+
+                //if (/*order is opened*/)
+                    return this;
+                //else
+                //    return _strat.States[nameof(MonitoringState)];
+            }
+        }
+    }
+}
