8b92ff9b9d7e7b5f174c7e41d9758e2f2d4b0da1
olgrea <olgrea@gmail.com>
2022-09-07 13:56:34 -0400

    moved indicators + strategy refactor WIP

---
+++ Strategies/StrategyBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradingBot.Strategies
+{
+    public abstract class StrategyBase : IStrategy
+    {
+        IState _currentState;
+
+        public StrategyBase(Trader trader)
+        {
+            Trader = trader;
+        }
+
+        public Trader Trader { get; private set; }
+        public Dictionary<string, IState> States { get; protected set; }
+
+        public IState CurrentState
+        {
+            get => _currentState;
+            set
+            {
+                if (value != _currentState)
+                {
+                    _currentState = value;
+                }
+            }
+        }
+
+        public abstract void Start();
+        public abstract void Stop();
+        
+    }
+}
