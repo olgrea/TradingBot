c1199f0c06e35b7a7ae9e6794a3772770789e502
olgrea <olgrea@gmail.com>
2022-09-09 13:09:01 -0400

    implemented RSIDivergence indicator (untested)

--- Indicators/RSI.cs
+++ Indicators/RSI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace TradingBot.Indicators
@@ -9,13 +10,17 @@ namespace TradingBot.Indicators
         const double _oversoldThreshold = 20.0;
         const double _overboughtThreshold = 80.0;
 
+        double _lastAvgU = double.MinValue;
+        double _lastAvgD = double.MinValue;
+
         LinkedList<double> _diffs = new LinkedList<double>();
 
         public RSI(int nbPeriods) : base(nbPeriods) { }
 
-        public double Value { get; protected set; }
+        public double Value { get; protected set; } = double.MinValue;
         public bool IsOverbought => Value > _overboughtThreshold;
         public bool IsOversold => Value < _oversoldThreshold;
+        public override bool IsReady => base.IsReady && Value != double.MinValue;
 
         public override void Compute()
         {
@@ -24,11 +29,29 @@ namespace TradingBot.Indicators
 
             // https://www.macroption.com/rsi-calculation/
             UpdateBarToBarChange();
-            var avgU = _diffs.Sum(d => Math.Max(d, 0)) / NbPeriods;
-            var avgD = _diffs.Sum(d => -Math.Min(-d, 0)) / NbPeriods;
-            var RS = avgU / avgD;
+            if (!base.IsReady)
+                return;
+
+            Debug.Assert(_diffs.Count == NbPeriods - 1);
+            var upMoves = _diffs.Select(d => Math.Max(d, 0));
+            var downMoves = _diffs.Select(d => -Math.Min(-d, 0));
             
-            Value = 100 - 100/(1+RS);
+            if (_lastAvgD == double.MinValue)
+            {
+                _lastAvgD = downMoves.Sum() / NbPeriods;
+                _lastAvgU = upMoves.Sum() / NbPeriods;
+            }
+            else
+            {
+                var a = 2 / (NbPeriods + 1);
+                var avgU = a * _diffs.Last.Value + (1 - a) * _lastAvgU;
+                var avgD = a * _diffs.Last.Value + (1 - a) * _lastAvgD;
+                var RS = avgU / avgD;
+                Value = 100 - 100/(1+RS);
+
+                _lastAvgD = avgD;
+                _lastAvgU = avgU;
+            }
         }
 
         void UpdateBarToBarChange()
