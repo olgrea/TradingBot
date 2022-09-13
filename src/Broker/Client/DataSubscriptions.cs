f5b4e966c4c8e966d515f0c875802c9fe94a002d
olgrea <olgrea@gmail.com>
2022-09-13 10:28:23 -0400

    moved all TWS callbacks into seperate class

---
+++ src/Broker/Client/DataSubscriptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TradingBot.Broker.Client
+{
+    internal class DataSubscriptions
+    {
+        public bool AccountUpdates { get; set; }
+        public bool Positions { get; set; }
+        public Dictionary<Contract, int> BidAsk { get; set; } = new Dictionary<Contract, int>();
+        public Dictionary<Contract, int> FiveSecBars{ get; set; } = new Dictionary<Contract, int>();
+        public Dictionary<Contract, int> Pnl { get; set; } = new Dictionary<Contract, int>();
+    }
+}
---
+++ src/Broker/Client/IBClient.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IBApi;
+using TradingBot.Utils;
+
+namespace TradingBot.Broker.Client
+{
+    internal class IBClient : IClient
+    {
+        static HashSet<int> _clientIds = new HashSet<int>();
+
+        const int DefaultPort = 7496;
+        const string DefaultIP = "127.0.0.1";
+        int _clientId = 1337;
+
+        int _nextValidOrderId = -1;
+        int _reqId = 0;
+
+        EClientSocket _clientSocket;
+        EReaderSignal _signal;
+        EReader _reader;
+        TWSCallbacks _IBCallbacks;
+
+        ILogger _logger;
+        Task _processMsgTask;
+
+        public IBClient(int clientId, ILogger logger)
+        {
+            _clientId = clientId;
+            _logger = logger;
+        }
+    }
+}
