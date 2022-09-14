522fa9b17806dd09cd2b37c22d3536785fee2c69
olgrea <olgrea@gmail.com>
2022-09-13 20:30:04 -0400

    initial commit of a historical data fetcher

---
+++ src/Utils/Serialization.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using TradingBot.Broker.MarketData;
+
+namespace TradingBot.Utils
+{
+    internal class Serialization
+    {
+        public static TCollection DeserializeBars<TCollection>(string path) where TCollection : IEnumerable<Bar>
+        {
+            var json = File.ReadAllText(path);
+            return (TCollection)JsonSerializer.Deserialize(json, typeof(TCollection));
+        }
+
+        public static void SerializeBars<TCollection>(string path, TCollection bars) where TCollection : IEnumerable<Bar>
+        {
+            var json = JsonSerializer.Serialize(bars);
+            File.WriteAllText(path, json);
+        }
+    }
+}
