dae8e68ffdf2709d3f7833411c2577758ac1db99
olgrea <olgrea@gmail.com>
2022-09-15 09:24:00 -0400

    one source of time truth : passing time Task. Also producer-consumer for messages

--- Backtester/FakeClient.cs
+++ Backtester/FakeClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -21,24 +22,26 @@ namespace Backtester
         {
             public static double TimeScale = 1;
             public static int OneSecond = (int)Math.Round(1 * 1000 * TimeScale);
-            public static int AccountUpdateInterval => (int)Math.Round(3 * 60 * 1000 * TimeScale);
         }
 
+        ConcurrentQueue<Action> _messageQueue;
+        Task _consumerTask;
+        CancellationTokenSource _consumerTaskCancellation;
+
+        DateTime _start;
+        DateTime _end;
+        DateTime _currentFakeTime;
+        event Action<DateTime> ClockTick;
         Task _passingTimeTask;
         CancellationTokenSource _passingTimeCancellation;
 
+        DateTime _lastAccountUpdate;
         Account _fakeAccount;
-        Task _accountUpdateTask;
-        CancellationTokenSource _accountUpdateCancellation;
-        CancellationTokenSource _accountUpdateDelayCancellation;
 
         //TODO : use IBApi classes?
         HashSet<Order> _openOrders;
         HashSet<Order> _executedOrders;
-        
-        DateTime _start;
-        DateTime _end;
-        DateTime _currentFakeTime;
+
         LinkedList<Bar> _dailyBars;
         LinkedListNode<Bar> _currentBarNode;
 
@@ -47,7 +50,6 @@ namespace Backtester
         IBClient _client;
 
         int _reqId5SecBar = -1;
-        bool _enable5SecBar = false;
 
         public FakeClient(DateTime start, DateTime end, IEnumerable<Bar> dailyBars, IBClient client, ILogger logger)
         {
@@ -58,6 +60,7 @@ namespace Backtester
             _currentBarNode = _dailyBars.First;
             _client = client;
             _logger = logger;
+            _messageQueue = new ConcurrentQueue<Action>();
 
             _fakeAccount = new Account()
             {
@@ -73,13 +76,86 @@ namespace Backtester
 
         public void Start()
         {
+            StartConsumerTask();
             StartPassingTimeTask();
         }
 
         public void Stop()
         {
+            CancelSubscriptions();
             StopPassingTimeTask();
-            StopAccountUpdateTask();
+            StopConsumerTask();
+        }
+
+        void CancelSubscriptions()
+        {
+            ClockTick -= OnClockTick_AccountSubscription;
+            ClockTick -= OnClockTick_FiveSecondBar;
+        }
+
+        void StartConsumerTask()
+        {
+            _consumerTaskCancellation = new CancellationTokenSource();
+            var token = _consumerTaskCancellation.Token;
+            _consumerTask = Task.Factory.StartNew(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    if (_messageQueue.TryDequeue(out Action action))
+                        action.Invoke();
+                }
+
+            }, token);
+        }
+
+        void StopConsumerTask()
+        {
+            _consumerTaskCancellation.Cancel();
+            _consumerTaskCancellation.Dispose();
+            _consumerTaskCancellation = null;
+            _consumerTask = null;
+        }
+
+        void StartPassingTimeTask()
+        {
+            _passingTimeCancellation = new CancellationTokenSource();
+            var mainToken = _passingTimeCancellation.Token;
+            _passingTimeTask = Task.Factory.StartNew(() =>
+            {
+                var delayCancellation = CancellationTokenSource.CreateLinkedTokenSource(mainToken, CancellationToken.None);
+                var delayToken = delayCancellation.Token;
+
+                while (!mainToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        ClockTick?.Invoke(_currentFakeTime);
+                        Task.Delay(TimeDelays.OneSecond, delayToken).Wait();
+                        
+                        _currentFakeTime = _currentFakeTime.AddSeconds(1);
+                        if (_currentFakeTime.Second % 5 == 0)
+                            _currentBarNode = _currentBarNode.Next;
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        delayCancellation.Dispose();
+                        if (!mainToken.IsCancellationRequested)
+                        {
+                            delayCancellation = CancellationTokenSource.CreateLinkedTokenSource(mainToken, CancellationToken.None);
+                            delayToken = delayCancellation.Token;
+                        }
+                    }
+                }
+
+            }, mainToken);
+        }
+
+        void StopPassingTimeTask()
+        {
+            _passingTimeCancellation.Cancel();
+            _passingTimeCancellation.Dispose();
+            _passingTimeCancellation = null;
+            _passingTimeTask = null;
         }
 
         public void Connect(string host, int port, int clientId)
@@ -117,15 +193,43 @@ namespace Backtester
 
         public void RequestAccount(string accountCode, bool receiveUpdates = true)
         {
-            SendAccountUpdate(accountCode);
+            if (accountCode != _fakeAccount.Code)
+                throw new InvalidOperationException();
+
+            _messageQueue.Enqueue(SendAccountUpdate);
+
             if (receiveUpdates)
             {
-                StartAccountUpdateTask(accountCode);
+                _lastAccountUpdate = _currentFakeTime;
+                ClockTick += OnClockTick_AccountSubscription;
             }
             else
             {
-                StopAccountUpdateTask();
+                ClockTick -= OnClockTick_AccountSubscription;
             }
+        }
+
+        void OnClockTick_AccountSubscription(DateTime newTime)
+        {
+            if(newTime -_lastAccountUpdate >= TimeSpan.FromSeconds(3))
+            {
+                SendAccountUpdate();
+            }
+        }
+
+        void ForceAccountUpdate() => SendAccountUpdate();
+
+        void SendAccountUpdate()
+        {
+            var currentTime = _currentFakeTime;
+            _lastAccountUpdate = currentTime;
+            _messageQueue.Enqueue(() => 
+            {
+                //TODO : insert delay between calls?
+                Callbacks.updateAccountTime(currentTime.ToString()); //TODO : make sure format is correct
+                Callbacks.updateAccountValue("CashBalance", _fakeAccount.CashBalances.First().Value.ToString(), "USD", _fakeAccount.Code);
+                Callbacks.accountDownloadEnd(_fakeAccount.Code);
+            });
         }
 
         public void RequestContract(int reqId, Contract contract)
@@ -135,8 +239,12 @@ namespace Backtester
 
             var cd = new IBApi.ContractDetails();
             cd.Contract = _contract.ToIBApiContract();
-            Callbacks.contractDetails(reqId, cd);
-            Callbacks.contractDetailsEnd(reqId);
+
+            _messageQueue.Enqueue(() => 
+            { 
+                Callbacks.contractDetails(reqId, cd);
+                Callbacks.contractDetailsEnd(reqId);
+            });
         }
 
         public void RequestFiveSecondsBars(int reqId, Contract contract)
@@ -145,20 +253,34 @@ namespace Backtester
                 throw new InvalidOperationException();
 
             _reqId5SecBar = reqId;
-            _enable5SecBar = true;
+            ClockTick += OnClockTick_FiveSecondBar;
+        }
+
+        void OnClockTick_FiveSecondBar(DateTime newTime)
+        {
+            var b = _currentBarNode.Value;
+            _messageQueue.Enqueue(() => 
+            {
+                DateTimeOffset dto = new DateTimeOffset(b.Time.ToUniversalTime());
+                Callbacks.realtimeBar(_reqId5SecBar, dto.ToUnixTimeSeconds(), b.Open, b.High, b.Low, b.Close, b.Volume, 0, b.TradeAmount);
+            });
         }
 
         public void CancelFiveSecondsBarsRequest(int reqId)
         {
             _reqId5SecBar = -1;
-            _enable5SecBar = false;
+            ClockTick -= OnClockTick_FiveSecondBar;
         }
 
         public void RequestOpenOrders()
         {
-            foreach (var o in _openOrders)
-                Callbacks.openOrder(o.Id, _contract.ToIBApiContract(), o.ToIBApiOrder(), new IBApi.OrderState() { Status = "Submitted" });
-            Callbacks.openOrderEnd();
+            var openOrders = _openOrders;
+            _messageQueue.Enqueue(() =>
+            {
+                foreach (var o in openOrders)
+                    Callbacks.openOrder(o.Id, _contract.ToIBApiContract(), o.ToIBApiOrder(), new IBApi.OrderState() { Status = "Submitted" });
+                Callbacks.openOrderEnd();
+            });
         }
 
         public void RequestPnL(int reqId, string accountCode, int contractId)
@@ -199,112 +321,6 @@ namespace Backtester
         public void RequestValidOrderIds()
         {
             throw new NotImplementedException();
-        }
-
-        void StartPassingTimeTask()
-        {
-            _passingTimeCancellation = new CancellationTokenSource();
-            var mainToken = _passingTimeCancellation.Token;
-            _passingTimeTask = Task.Factory.StartNew(() =>
-            {
-                var delayCancellation = CancellationTokenSource.CreateLinkedTokenSource(mainToken, CancellationToken.None);
-                var delayToken = delayCancellation.Token;
-
-                while (!mainToken.IsCancellationRequested)
-                {
-                    try
-                    {
-                        ClockTick(_currentFakeTime);
-                        Task.Delay(TimeDelays.OneSecond, delayToken).Wait();
-                        _currentFakeTime = _currentFakeTime.AddSeconds(1);
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        delayCancellation.Dispose();
-                        if(!mainToken.IsCancellationRequested)
-                        {
-                            delayCancellation = CancellationTokenSource.CreateLinkedTokenSource(mainToken, CancellationToken.None);
-                            delayToken = delayCancellation.Token;
-                        }
-                    }
-                }
-
-            }, mainToken);
-        }
-
-        void StopPassingTimeTask()
-        {
-            _passingTimeCancellation.Cancel();
-            _passingTimeCancellation.Dispose();
-            _passingTimeCancellation = null;
-            _passingTimeTask = null;
-        }
-
-        void ClockTick(DateTime newTime)
-        {
-            if(newTime.Second % 5 == 0)
-            {
-                _currentBarNode = _currentBarNode.Next;
-                if(_enable5SecBar)
-                {
-                    var b = _currentBarNode.Value;
-                    DateTimeOffset dto = new DateTimeOffset(b.Time.ToUniversalTime());
-                    Callbacks.realtimeBar(_reqId5SecBar, dto.ToUnixTimeSeconds(), b.Open, b.High, b.Low, b.Close, b.Volume, 0, b.TradeAmount);
-                }
-            }
-        }
-
-        void StopAccountUpdateTask()
-        {
-            _accountUpdateCancellation.Cancel();
-            _accountUpdateCancellation.Dispose();
-            _accountUpdateDelayCancellation.Cancel();
-            _accountUpdateDelayCancellation.Dispose();
-            _accountUpdateCancellation = null;
-            _accountUpdateDelayCancellation = null;
-            _accountUpdateTask = null;
-        }
-
-        void StartAccountUpdateTask(string accountCode)
-        {
-            _accountUpdateCancellation = new CancellationTokenSource();
-            var mainToken = _accountUpdateCancellation.Token;
-            _accountUpdateTask = Task.Factory.StartNew(() =>
-            {
-                _accountUpdateDelayCancellation = CancellationTokenSource.CreateLinkedTokenSource(mainToken, CancellationToken.None);
-                var delayToken = _accountUpdateDelayCancellation.Token;
-
-                while (!mainToken.IsCancellationRequested)
-                {
-                    try
-                    {
-                        Task.Delay(TimeDelays.AccountUpdateInterval, delayToken).Wait();
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        _accountUpdateDelayCancellation.Dispose();
-                        if(!mainToken.IsCancellationRequested)
-                        {
-                            _accountUpdateDelayCancellation = CancellationTokenSource.CreateLinkedTokenSource(mainToken, CancellationToken.None);
-                            delayToken = _accountUpdateDelayCancellation.Token;
-                        }
-                    }
-
-                    if(!mainToken.IsCancellationRequested)
-                        SendAccountUpdate(accountCode);
-                }
-
-            }, mainToken);
-        }
-
-        void ForceAccountUpdate() => _accountUpdateDelayCancellation.Cancel();
-
-        void SendAccountUpdate(string accountCode)
-        {
-            //TODO : insert delay between calls?
-            Callbacks.updateAccountTime(_currentFakeTime.ToString()); //TODO : make sure format is correct
-            Callbacks.updateAccountValue("CashBalance", _fakeAccount.CashBalances.First().Value.ToString(), "USD", _fakeAccount.Code);
-            Callbacks.accountDownloadEnd(accountCode);
         }
     }
 }
