ef13de1a2ea161a3a9b5ce81234c871f0d320b0e
olgrea <olgrea@gmail.com>
2022-09-19 15:29:08 -0400

    initial commit for Tests + added nlog

---
+++ Tests/Backtester/FakeClientTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+
+namespace Tests.Backtester
+{
+    [TestClass]
+    public class FakeClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
