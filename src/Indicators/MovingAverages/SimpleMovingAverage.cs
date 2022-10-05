00340c4347bca36ec35d5734f0350ab0567b8115
olgrea <olgrea@gmail.com>
2022-10-05 09:05:19 -0400

    created base abstract class for moving averages

---
+++ src/Indicators/MovingAverages/SimpleMovingAverage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingBot.Broker.MarketData;
+
+// TODO : encapsulate this ?
+using MathNet.Numerics.Statistics;
+using System.Linq;
+
+namespace TradingBot.Indicators.MovingAverages
+{
+    internal class SimpleMovingAverage : MovingAverage
+    {
+        public SimpleMovingAverage(BarLength barLength, int nbPeriods) : base(barLength, nbPeriods) { }
+
+        public override double ComputeMA()
+        {
+            var barsTypicalPrice = Bars.Select(b => (b.Close + b.High + b.Low) / 3);
+            return barsTypicalPrice.Mean();
+        }
+    }
+}
