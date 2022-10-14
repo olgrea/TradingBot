e9ee1ec910d2da1229967331177a46f8ed833681
olgrea <olgrea@gmail.com>
2022-10-14 08:15:34 -0400

    Connect_CanBeCancelled test

--- Tests/Client/IBClientTests.cs
+++ Tests/Client/IBClientTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NLog;
 using NUnit.Framework;
@@ -27,6 +28,13 @@ namespace Tests.Client
             _client = new IBClient(_logger);
         }
 
+        [SetUp]
+        public async Task SetUp()
+        {
+            var msg = await _client.ConnectAsync(DefaultIP, DefaultPort, DefaultClientId);
+            Assert.IsTrue(msg.AccountCode == "DU5962304");
+        }
+
         [TearDown]
         public async Task TearDown()
         {
@@ -37,6 +45,10 @@ namespace Tests.Client
         [Test]
         public async Task Connect_OnSuccess_ReturnsRelevantConnectionInfo()
         {
+            // Setup
+            await _client.DisconnectAsync();
+            await Task.Delay(50);
+
             // Test
             var msg = await _client.ConnectAsync(DefaultIP, DefaultPort, DefaultClientId);
 
@@ -49,8 +61,25 @@ namespace Tests.Client
         [Test]
         public async Task Connect_AlreadyConnected_ThrowsError()
         {
+            // Setup
+            await _client.DisconnectAsync();
+            await Task.Delay(50);
             await _client.ConnectAsync(DefaultIP, DefaultPort, DefaultClientId);
+            
+            // Test
             Assert.ThrowsAsync<ErrorMessage>(async () => await _client.ConnectAsync(DefaultIP, DefaultPort, DefaultClientId)); 
+        }
+
+        [Test]
+        public async Task Connect_CanBeCancelled()
+        {
+            // Setup
+            await _client.DisconnectAsync();
+            await Task.Delay(50);
+
+            // Test
+            var source = new CancellationTokenSource(5);
+            Assert.ThrowsAsync<TaskCanceledException>(async () => await _client.ConnectAsync(DefaultIP, DefaultPort, DefaultClientId, source.Token));
         }
     }
 }
