a1e4eaa7348562bc08f838374f1867bca6bd8179
olgrea <olgrea@gmail.com>
2022-10-25 11:16:57 -0400

    created LinkedListWithMaxSize class

---
+++ src/Utils/LinkedListUtils.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Collections.Generic;
+
+namespace TradingBot.Utils
+{
+    public class LinkedListWithMaxSize<T> : LinkedList<T>
+    {
+        int _maxSize;
+        public LinkedListWithMaxSize(int maxSize)
+        {
+            _maxSize = maxSize;
+        }
+
+        public void Add(T item)
+        {
+            AddLast(item);
+            if (Count > _maxSize)
+                RemoveFirst();
+        }
+    }
+}
