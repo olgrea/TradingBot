f6b5ab9dec6991eb51d8bdc5b68141c67a427faa
olgrea <olgrea@gmail.com>
2022-10-25 16:22:20 -0400

    moved HistoricalDataFetcher to MarketDataUtils

--- HistoricalDataFetcher/Program.cs
+++ HistoricalDataFetcher/Program.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using CommandLine;
 using NLog;
 using TradingBot.Broker;
-using TradingBot.Broker.Client;
-using TradingBot.Broker.Client.Messages;
 using TradingBot.Broker.MarketData;
-using TradingBot.Utils;
+using static TradingBot.Utils.MarketDataUtils;
+using static TradingBot.Utils.MarketDataUtils.HistoricalDataFetcher;
 
-[assembly: InternalsVisibleToAttribute("Tests")]
+[assembly: InternalsVisibleTo("Tests")]
 namespace HistoricalDataFetcher
 {
     internal class Program
@@ -38,254 +36,41 @@ namespace HistoricalDataFetcher
             if (!string.IsNullOrEmpty(parsedArgs.Value.EndDate))
                 endDate = DateTime.Parse(parsedArgs.Value.EndDate);
 
-            var hdf = new DataFetcher(ticker, startDate, endDate);    
-            await hdf.Start();
-        }
-    }
-
-    internal class DataFetcher
-    {
-        public const string RootDir = MarketDataUtils.RootDir;
-        public const string DbPath = "C:\\tradingbot\\db\\historicaldata.sqlite3";
-
-        public int _nbRequest = 0;
-
-        public int NbRequest
-        {
-            get => _nbRequest;
-            set
-            {
-                _nbRequest = value;
-                CheckForPacingViolations();
-            }
+            await FetchEverything(ticker, startDate.AddTicks(MarketStartTime.Ticks), endDate.AddTicks(MarketEndTime.Ticks));
         }
 
-        public IBBroker _broker;
-        public ILogger _logger;
-
-        string _ticker;
-        DateTime _startDate;
-        DateTime _endDate;
-        IErrorHandler _errorHandler;
-
-        internal class FetcherErrorHandler : IBBrokerErrorHandler
+        public static async Task FetchEverything(string ticker, DateTime startDate, DateTime endDate)
         {
-            DataFetcher _fetcher;
-            public FetcherErrorHandler(DataFetcher fetcher, IBBroker broker, ILogger logger) : base(broker, logger)
-            {
-                _fetcher = fetcher;
-            }
+            var broker = new IBBroker(321);
+            var logger = LogManager.GetLogger($"{nameof(TradingBot.Utils.MarketDataUtils.HistoricalDataFetcher)}");
+            var dataFetcher = new TradingBot.Utils.MarketDataUtils.HistoricalDataFetcher(broker, logger);
+            broker.ErrorHandler = new FetcherErrorHandler(dataFetcher, broker, logger); 
 
-            public override bool IsHandled(ErrorMessageException msg)
-            {
-                switch (msg.ErrorCode)
-                {
-                    // TODO : handle pacing violation for when the program has been started and restarted a lot
-                    default:
-                        //_fetcher.Wait10Minutes();
-                        return base.IsHandled(msg);
-                }
-            }
-        }
-
-        public DataFetcher(string ticker, DateTime start, DateTime end)
-        {
-            _ticker = ticker;
-
-            var marketStartTime = MarketDataUtils.MarketStartTime;
-            var marketEndTime = MarketDataUtils.MarketEndTime;
-            _startDate = new DateTime(start.Year, start.Month, start.Day, 8, 0, 0);
-            _endDate = new DateTime(end.Year, end.Month, end.Day, marketEndTime.Hours, marketEndTime.Minutes, marketEndTime.Seconds);
-
-            _logger = LogManager.GetLogger($"{nameof(DataFetcher)}");
-            _broker = new IBBroker(321);
-
-            _errorHandler = new FetcherErrorHandler(this, _broker as IBBroker, _logger);
-            _broker.ErrorHandler = _errorHandler;
-
-        }
-
-        public async Task Start()
-        {
-            await _broker.ConnectAsync();
-
-            var contract = await _broker.GetContractAsync(_ticker);
+            await broker.ConnectAsync();
+            var contract = await broker.GetContractAsync(ticker);
             if (contract == null)
-                throw new ArgumentException($"can't find contract for ticker {_ticker}");
+                throw new ArgumentException($"can't find contract for ticker {ticker}");
 
-            var marketDays = MarketDataUtils.GetMarketDays(_startDate, _endDate).ToList();
+            var marketDays = GetMarketDays(startDate, endDate).ToList();
             foreach ((DateTime, DateTime) pair in marketDays)
-                await GetDataForDay<Bar>(pair.Item1, contract);
+            {
+                try
+                {
+                    await dataFetcher.GetDataForDay<Bar>(pair.Item1.Date, (pair.Item1.TimeOfDay, pair.Item2.TimeOfDay), contract);
+                }
+                catch (MarketHolidayException) { break; }
+            }
 
             foreach ((DateTime, DateTime) pair in marketDays)
-                await GetDataForDay<BidAsk>(pair.Item1, contract);
-
-            _logger.Info($"\nComplete!\n");
-        }
-
-        async Task GetDataForDay<TData>(DateTime date, Contract contract) where TData : IMarketData, new()
-        {
-            var marketStart = MarketDataUtils.MarketStartTime;
-            var marketEnd = MarketDataUtils.MarketEndTime;
-
-            DateTime morning = new DateTime(date.Year, date.Month, date.Day, marketStart.Hours, marketStart.Minutes, marketStart.Seconds, DateTimeKind.Local);
-            DateTime current = new DateTime(date.Year, date.Month, date.Day, marketEnd.Hours, marketEnd.Minutes, marketEnd.Seconds, DateTimeKind.Local);
-
-            _logger.Info($"Getting data for {contract.Symbol} on {date.ToShortDateString()} ({morning.ToShortTimeString()} to {current.ToShortTimeString()})");
-
-            while (current >= morning)
             {
-                var begin = current.AddMinutes(-30);
-                var end = current;
-                if (DataExists<TData>(contract.Symbol, current.Date, (begin.TimeOfDay, end.TimeOfDay)))
+                try
                 {
-                    var dateStr = current.Date.ToShortDateString();
-                    _logger.Info($"Data for {contract.Symbol} {dateStr} ({begin.ToShortTimeString()}-{end.ToShortTimeString()}) already exists in db. Skipping.");
+                    await dataFetcher.GetDataForDay<BidAsk>(pair.Item1.Date, (pair.Item1.TimeOfDay, pair.Item2.TimeOfDay), contract);
                 }
-                else
-                {
-                    try
-                    {
-                        LinkedList<TData> data = await FetchHistoricalData<TData>(contract, current);
-                        SaveData(contract.Symbol, current, data);
-                    }
-                    catch (MarketHolidayException) { break; }
-                }
-
-                current = current.AddMinutes(-30);
-            }
-        }
-
-        private void SaveData<TData>(string symbol, DateTime date, IEnumerable<TData> dailyData) where TData : IMarketData, new()
-        {
-            DbUtils.InsertData<TData>(symbol, dailyData);
-        }
-
-        private void CheckForPacingViolations()
-        {
-            // TWS API limitations. Pacing violation occurs when : 
-            // - Making identical historical data requests within 15 seconds.
-            // - Making six or more historical data requests for the same Contract, Exchange and Tick Type within two seconds.
-            // - Making more than 60 requests within any ten minute period.
-            // https://interactivebrokers.github.io/tws-api/historical_limitations.html
-
-            if (_nbRequest == 60)
-            {
-                _logger.Info($"60 requests made : waiting 10 minutes...");
-                Wait10Minutes();
-                _nbRequest = 0;
-            }
-            else if (_nbRequest != 0 && _nbRequest % 5 == 0)
-            {
-                _logger.Info($"{NbRequest} requests made : waiting 2 seconds...");
-                Task.Delay(2000).Wait();
-            }
-        }
-
-        void Wait10Minutes()
-        {
-            for (int i = 0; i < 10; ++i)
-            {
-                Task.Delay(60 * 1000).Wait();
-                if (i < 9)
-                    _logger.Info($"{9 - i} minutes left...");
-                else
-                    _logger.Info($"Resuming historical data fetching");
-            }
-        }
-
-        class MarketHolidayException : Exception { }
-
-        async Task<LinkedList<TData>> FetchHistoricalData<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
-        {
-            LinkedList<TData> data = await Fetch<TData>(contract, time);
-            if (IsPossibleMarketHoliday(time, data))
-            {
-                _logger.Info($"Possible market holiday on {time} (returned data time mismatch). Skipping.");
-                throw new MarketHolidayException();
+                catch (MarketHolidayException) { break; }
             }
 
-            return data;
-        }
-
-        private bool DataExists<TData>(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange) where TData : IMarketData, new()
-        {
-            return DbUtils.DataExistsInDb<TData>(symbol, date, timeRange);
-        }
-
-        bool IsPossibleMarketHoliday<TData>(DateTime time, IEnumerable<TData> data) where TData : IMarketData, new()
-        {
-            // TODO : better way to know if the market was opened or not?
-            // On market holidays, TWS seems to return the bars of the previous trading day.
-            var d = data.FirstOrDefault();
-            return d != null && time.Date != d.Time.Date;
-        }
-
-        async Task<LinkedList<TData>> Fetch<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
-        {
-            if (typeof(TData) == typeof(Bar))
-            {
-                return await FetchBars<TData>(contract, time);
-            }
-            else if (typeof(TData) == typeof(BidAsk))
-            {
-                return await FetchBidAsk<TData>(contract, time);
-            }
-
-            return new LinkedList<TData>();
-        }
-
-        private async Task<LinkedList<TData>> FetchBidAsk<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
-        {
-            _logger.Info($"Retrieving bid ask from TWS for '{contract.Symbol} {time}'.");
-
-            // max nb of ticks per request is 1000 so we need to do multiple requests for 30 minutes...
-            // There doesn't seem to be a way to convert ticks to seconds... 1 tick != 1 seconds apparently. 
-            // So we just do requests as long as we don't have 30 minutes.
-            IEnumerable<BidAsk> bidask = new LinkedList<BidAsk>();
-            DateTime current = time;
-            TimeSpan _30min = TimeSpan.FromMinutes(30);
-            //TimeSpan _20min = TimeSpan.FromMinutes(20);
-            var diff = time - current;
-            int tickCount = 1000;
-            while (diff <= _30min)
-            {
-                //// Adjusting tick count for the last 5 minutes in order to not retrieve too much out of range data...
-                //if(diff > _20min)
-                //    tickCount = 100;
-
-                var ticks = await _broker.RequestHistoricalTicks(contract, current, tickCount);
-
-                // Note that when BID_ASK historical data is requested, each request is counted twice according to the doc
-                NbRequest++; NbRequest++;
-                if (IsPossibleMarketHoliday(current, ticks))
-                    return new LinkedList<TData>();
-
-                bidask = ticks.Concat(bidask);
-                current = ticks.First().Time;
-
-                diff = time - current;
-            }
-
-            // Remove out of range data.
-            var list = new LinkedList<TData>(bidask.Cast<TData>());
-            var currNode = list.First;
-            var timeOfDay = (time - _30min).TimeOfDay;
-            while (currNode != null && currNode.Value.Time.TimeOfDay < timeOfDay)
-            {
-                list.RemoveFirst();
-                currNode = list.First;
-            }
-
-            return list;
-        }
-
-        private async Task<LinkedList<TData>> FetchBars<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
-        {
-            _logger.Info($"Retrieving bars from TWS for '{contract.Symbol} {time}'.");
-            var bars = await _broker.GetHistoricalDataAsync(contract, BarLength._1Sec, time, 1800);
-            NbRequest++;
-            return new LinkedList<TData>(bars.Cast<TData>());
+            logger.Info($"\nComplete!\n");
         }
     }
 }
--- src/Utils/MarketDataUtils.cs
+++ src/Utils/MarketDataUtils.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
+using NLog;
+using System.Threading.Tasks;
+using TradingBot.Broker.Client.Messages;
+using TradingBot.Broker.Client;
+using TradingBot.Broker;
 using TradingBot.Broker.MarketData;
 
 namespace TradingBot.Utils
@@ -28,8 +34,8 @@ namespace TradingBot.Utils
             if (end <= start)
                 yield break;
 
-            DateTime marketStartTime = new DateTime(start.Year, start.Month, start.Day, MarketStartTime.Hours, MarketStartTime.Minutes, MarketStartTime.Seconds);
-            DateTime marketEndTime = new DateTime(start.Year, start.Month, start.Day, MarketEndTime.Hours, MarketEndTime.Minutes, MarketEndTime.Seconds);
+            DateTime marketStartTime = new DateTime(start.Date.Ticks + MarketStartTime.Ticks);
+            DateTime marketEndTime = new DateTime(start.Date.Ticks + MarketEndTime.Ticks);
 
             if (start < marketStartTime)
                 start = marketStartTime;
@@ -125,6 +131,216 @@ namespace TradingBot.Utils
             }
 
             return bar;
+        }
+
+        internal class HistoricalDataFetcher
+        {
+            public int _nbRequest = 0;
+
+            public int NbRequest
+            {
+                get => _nbRequest;
+                set
+                {
+                    _nbRequest = value;
+                    CheckForPacingViolations();
+                }
+            }
+
+            public IBBroker _broker;
+            public ILogger _logger;
+
+            internal class FetcherErrorHandler : IBBrokerErrorHandler
+            {
+                HistoricalDataFetcher _fetcher;
+                public FetcherErrorHandler(HistoricalDataFetcher fetcher, IBBroker broker, ILogger logger) : base(broker, logger)
+                {
+                    _fetcher = fetcher;
+                }
+
+                public override bool IsHandled(ErrorMessageException msg)
+                {
+                    switch (msg.ErrorCode)
+                    {
+                        // TODO : handle pacing violation for when the program has been started and restarted a lot
+                        default:
+                            //_fetcher.Wait10Minutes();
+                            return base.IsHandled(msg);
+                    }
+                }
+            }
+
+            public HistoricalDataFetcher(IBBroker broker, ILogger logger)
+            {
+                _broker = broker ?? new IBBroker(321);
+                _logger = logger ?? LogManager.GetLogger($"{nameof(HistoricalDataFetcher)}");
+            }
+
+            public async Task<IEnumerable<TData>> GetDataForDay<TData>(DateTime date, (TimeSpan, TimeSpan) timeRange, Contract contract) where TData : IMarketData, new()
+            {
+                DateTime morning = new DateTime(date.Date.Ticks + timeRange.Item1.Ticks);
+                DateTime current = new DateTime(date.Date.Ticks + timeRange.Item2.Ticks);
+
+                _logger.Info($"Getting data for {contract.Symbol} on {date.ToShortDateString()} ({morning.ToShortTimeString()} to {current.ToShortTimeString()})");
+
+                // In order to respect TWS limitationsm, data is retrieved in chunks of 30 minutes for bars of 1 sec length (1800 bars total), from the end of the
+                // time range to the beginning. 
+                // https://interactivebrokers.github.io/tws-api/historical_limitations.html
+
+                IEnumerable<TData> dailyData = Enumerable.Empty<TData>();
+                while (current >= morning)
+                {
+                    var begin = current.AddMinutes(-30);
+                    var end = current;
+                    if (DataExists<TData>(contract.Symbol, current.Date, (begin.TimeOfDay, end.TimeOfDay)))
+                    {
+                        var dateStr = current.Date.ToShortDateString();
+                        _logger.Info($"Data for {contract.Symbol} {dateStr} ({begin.ToShortTimeString()}-{end.ToShortTimeString()}) already exists in db. Skipping.");
+                    }
+                    else
+                    {
+                        LinkedList<TData> data = await FetchHistoricalData<TData>(contract, current);
+                        dailyData = data.Concat(dailyData);
+                        SaveData(contract.Symbol, current, data);
+                    }
+
+                    current = current.AddMinutes(-30);
+                }
+
+                return dailyData;
+            }
+
+            private void SaveData<TData>(string symbol, DateTime date, IEnumerable<TData> dailyData) where TData : IMarketData, new()
+            {
+                DbUtils.InsertData<TData>(symbol, dailyData);
+            }
+
+            private void CheckForPacingViolations()
+            {
+                // TWS API limitations. Pacing violation occurs when : 
+                // - Making identical historical data requests within 15 seconds.
+                // - Making six or more historical data requests for the same Contract, Exchange and Tick Type within two seconds.
+                // - Making more than 60 requests within any ten minute period.
+                // https://interactivebrokers.github.io/tws-api/historical_limitations.html
+
+                if (_nbRequest == 60)
+                {
+                    _logger.Info($"60 requests made : waiting 10 minutes...");
+                    Wait10Minutes();
+                    _nbRequest = 0;
+                }
+                else if (_nbRequest != 0 && _nbRequest % 5 == 0)
+                {
+                    _logger.Info($"{NbRequest} requests made : waiting 2 seconds...");
+                    Task.Delay(2000).Wait();
+                }
+            }
+
+            void Wait10Minutes()
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    Task.Delay(60 * 1000).Wait();
+                    if (i < 9)
+                        _logger.Info($"{9 - i} minutes left...");
+                    else
+                        _logger.Info($"Resuming historical data fetching");
+                }
+            }
+
+            public class MarketHolidayException : Exception { }
+
+            async Task<LinkedList<TData>> FetchHistoricalData<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
+            {
+                LinkedList<TData> data = await Fetch<TData>(contract, time);
+                if (IsPossibleMarketHoliday(time, data))
+                {
+                    _logger.Info($"Possible market holiday on {time} (returned data time mismatch). Skipping.");
+                    throw new MarketHolidayException();
+                }
+
+                return data;
+            }
+
+            private bool DataExists<TData>(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange) where TData : IMarketData, new()
+            {
+                return DbUtils.DataExistsInDb<TData>(symbol, date, timeRange);
+            }
+
+            bool IsPossibleMarketHoliday<TData>(DateTime time, IEnumerable<TData> data) where TData : IMarketData, new()
+            {
+                // TODO : better way to know if the market was opened or not?
+                // On market holidays, TWS seems to return the bars of the previous trading day.
+                var d = data.FirstOrDefault();
+                return d != null && time.Date != d.Time.Date;
+            }
+
+            async Task<LinkedList<TData>> Fetch<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
+            {
+                if (typeof(TData) == typeof(Bar))
+                {
+                    return await FetchBars<TData>(contract, time);
+                }
+                else if (typeof(TData) == typeof(BidAsk))
+                {
+                    return await FetchBidAsk<TData>(contract, time);
+                }
+
+                return new LinkedList<TData>();
+            }
+
+            private async Task<LinkedList<TData>> FetchBidAsk<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
+            {
+                _logger.Info($"Retrieving bid ask from TWS for '{contract.Symbol} {time}'.");
+
+                // max nb of ticks per request is 1000 so we need to do multiple requests for 30 minutes...
+                // There doesn't seem to be a way to convert ticks to seconds...
+                // So we just do requests as long as we don't have 30 minutes.
+                IEnumerable<BidAsk> bidask = new LinkedList<BidAsk>();
+                DateTime current = time;
+                TimeSpan _30min = TimeSpan.FromMinutes(30);
+                //TimeSpan _20min = TimeSpan.FromMinutes(20);
+                var diff = time - current;
+                int tickCount = 1000;
+                while (diff <= _30min)
+                {
+                    //// Adjusting tick count for the last 5 minutes in order to not retrieve too much out of range data...
+                    //if(diff > _20min)
+                    //    tickCount = 100;
+
+                    var ticks = await _broker.RequestHistoricalTicks(contract, current, tickCount);
+
+                    // Note that when BID_ASK historical data is requested, each request is counted twice according to the doc
+                    NbRequest++; NbRequest++;
+                    if (IsPossibleMarketHoliday(current, ticks))
+                        return new LinkedList<TData>();
+
+                    bidask = ticks.Concat(bidask);
+                    current = ticks.First().Time;
+
+                    diff = time - current;
+                }
+
+                // Remove out of range data.
+                var list = new LinkedList<TData>(bidask.Cast<TData>());
+                var currNode = list.First;
+                var timeOfDay = (time - _30min).TimeOfDay;
+                while (currNode != null && currNode.Value.Time.TimeOfDay < timeOfDay)
+                {
+                    list.RemoveFirst();
+                    currNode = list.First;
+                }
+
+                return list;
+            }
+
+            private async Task<LinkedList<TData>> FetchBars<TData>(Contract contract, DateTime time) where TData : IMarketData, new()
+            {
+                _logger.Info($"Retrieving bars from TWS for '{contract.Symbol} {time}'.");
+                var bars = await _broker.GetHistoricalDataAsync(contract, BarLength._1Sec, time, 1800);
+                NbRequest++;
+                return new LinkedList<TData>(bars.Cast<TData>());
+            }
         }
     }
 }
