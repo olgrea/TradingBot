a378db65d8ed2cd918e716aa3cae6d2280587742
olgrea <olgrea@gmail.com>
2022-11-11 17:51:39 -0500

    IndicatorBase

--- TradingBot/Indicators/BBTrend.cs
+++ TradingBot/Indicators/BBTrend.cs
@@ -10,46 +10,36 @@ namespace TradingBot.Indicators
         public double? BBTrend { get; set; }
     }
 
-    public class BBTrend : IIndicator
+    internal class BBTrend : IndicatorBase<BBTrendResult>
     {
-        IEnumerable<BBTrendResult> _results;
-        IEnumerable<BBTrendResult> _trendingResults = Enumerable.Empty<BBTrendResult>();
-
-        BarLength _barLength;
         BollingerBands _slowBB;
         BollingerBands _fastBB;
 
-        public BBTrend(BarLength barLength, int slowPeriod = 50, int fastPeriod = 20)
+        public BBTrend(BarLength barLength, int slowPeriod = 50, int fastPeriod = 20) : base(barLength, slowPeriod, slowPeriod)
         {
-            _barLength = barLength;
             _slowBB = new BollingerBands(barLength, slowPeriod);
             _fastBB = new BollingerBands(barLength, fastPeriod);
         }
         
-        public BBTrendResult LatestResult => _results?.LastOrDefault();
-
-        public BarLength BarLength => _barLength;
-        public bool IsReady => LatestResult != null && _results.Count() >= _slowBB.NbWarmupPeriods;
-        public int NbPeriods => _slowBB.NbPeriods;
-        public int NbWarmupPeriods => _slowBB.NbWarmupPeriods;
-
-        public void Compute(IEnumerable<IQuote> quotes)
+        public override void Compute(IEnumerable<IQuote> quotes)
         {
+            base.Compute(quotes);
             _slowBB.Compute(quotes);
             _fastBB.Compute(quotes);
             _results = ComputeResults(_slowBB.Results, _fastBB.Results);
         }
 
-        public void ComputeTrend(IQuote partialQuote)
+        public override void ComputeTrend(Last last)
         {
-            _slowBB.ComputeTrend(partialQuote);
-            _fastBB.ComputeTrend(partialQuote);
+            base.ComputeTrend(last);
+            _slowBB.ComputeTrend(last);
+            _fastBB.ComputeTrend(last);
             _trendingResults = ComputeResults(_slowBB.TrendingResults, _fastBB.TrendingResults);
         }
 
-        IEnumerable<BBTrendResult> ComputeResults(IEnumerable<BollingerBandsResult> slowBBResults, IEnumerable<BollingerBandsResult> fastBBResults)
+        IEnumerable<BBTrendResult> ComputeResults(IEnumerable<IResult> slowBBResults, IEnumerable<IResult> fastBBResults)
         {
-            return slowBBResults.Zip(fastBBResults, (slow, fast) =>
+            return slowBBResults.Cast<BollingerBandsResult>().Zip(fastBBResults.Cast<BollingerBandsResult>(), (slow, fast) =>
             {
                 var lower = NullMath.Abs(fast.LowerBand - slow.LowerBand);
                 var upper = NullMath.Abs(fast.UpperBand - slow.UpperBand);
--- TradingBot/Indicators/BollingerBands.cs
+++ TradingBot/Indicators/BollingerBands.cs
@@ -1,49 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using InteractiveBrokers.MarketData;
 using Skender.Stock.Indicators;
 
 namespace TradingBot.Indicators
 {
-    internal class BollingerBands : IIndicator
+    internal class BollingerBands : IndicatorBase<BollingerBandsResult>
     {
-        IEnumerable<IQuote> _quotes;
-        IEnumerable<BollingerBandsResult> _results;
-
-        LinkedList<IQuote> _partialQuotes = new LinkedList<IQuote>();
-        IEnumerable<BollingerBandsResult> _trendingResults = Enumerable.Empty<BollingerBandsResult>();
-
-        BarLength _barLength;
-        int _nbPeriods;
-
-        public BollingerBands(BarLength barLength, int nbPeriods = 20)
+        public BollingerBands(BarLength barLength, int nbPeriods = 20) : base(barLength, nbPeriods, nbPeriods)
         {
-            _barLength = barLength;
-            _nbPeriods = nbPeriods;
-        }
-        
-        public BollingerBandsResult LatestResult => _results?.LastOrDefault();
-        public IEnumerable<BollingerBandsResult> Results => _results;
-
-        public BollingerBandsResult LatestTrendingResult => _trendingResults?.LastOrDefault();
-        public IEnumerable<BollingerBandsResult> TrendingResults => _trendingResults;
-
-        public BarLength BarLength => _barLength;
-        public bool IsReady => LatestResult != null && _results.Count() >= NbWarmupPeriods;
-        public int NbPeriods => _nbPeriods;
-        public int NbWarmupPeriods => _nbPeriods;
-
-        public void Compute(IEnumerable<IQuote> quotes)
-        {
-            _partialQuotes.Clear();
-            _quotes = quotes;
-            _results = ComputeResults(_quotes);
         }
 
-        public void ComputeTrend(IQuote partialQuote)
+        public override void Compute(IEnumerable<IQuote> quotes)
         {
-            _partialQuotes.AddLast(partialQuote);
-            _trendingResults = ComputeResults(_quotes.Concat(_partialQuotes));
+            base.Compute(quotes);
+            _results = ComputeResults(quotes);
+        }
+
+        public override void ComputeTrend(Last last)
+        {
+            base.ComputeTrend(last);
+            _trendingResults = ComputeResults(_quotes.Append(_partialQuote));
         }
 
         IEnumerable<BollingerBandsResult> ComputeResults(IEnumerable<IQuote> quotes)
