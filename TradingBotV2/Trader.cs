253a9fcbfb48c58f66a8639c22c0c3f2ad14815a
olgrea <olgrea@gmail.com>
2023-04-04 13:03:43 -0400

    disabled Nullable reference types for now

--- TradingBotV2/Trader.cs
+++ TradingBotV2/Trader.cs
@@ -4,6 +4,8 @@ using TradingBotV2.Broker;
 [assembly: InternalsVisibleTo("TradingBotV2.Tests")]
 namespace TradingBotV2
 {
+    // TODO : investigate that new "non-nullable" project setting
+
     public class Trader
     {
         IBroker broker;
