ee80a4ee4860a83734dd1d7c0853f7dde5d86d05
olgrea <olgrea@gmail.com>
2023-04-06 18:55:06 -0400

    Backtester IBroker tests

--- TradingBotV2/Backtesting/Backtester.cs
+++ TradingBotV2/Backtesting/Backtester.cs
@@ -1,34 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TradingBotV2.Broker;
+﻿using TradingBotV2.Broker;
 using TradingBotV2.Broker.Accounts;
 using TradingBotV2.Broker.MarketData;
 using TradingBotV2.Broker.Orders;
+using TradingBotV2.IBKR;
 
 namespace TradingBotV2.Backtesting
 {
     internal class Backtester : IBroker
     {
+        bool _isConnected = false;
+        Account _fakeAccount;
+
+        public Backtester()
+        {
+            _fakeAccount = new Account()
+            {
+                Code = "FAKEACCOUNT123",
+                CashBalances = new Dictionary<string, double>()
+                {
+                    { "BASE", 25000.00 },
+                    { "USD", 25000.00 },
+                },
+                UnrealizedPnL = new Dictionary<string, double>()
+                {
+                    { "BASE", 0.00},
+                    { "USD", 0.00 },
+                },
+                RealizedPnL = new Dictionary<string, double>()
+                {
+                    { "BASE", 0.00 },
+                    { "USD", 0.00 },
+                }
+            };
+        }
+
         public IMarketDataProvider MarketDataProvider => throw new NotImplementedException();
 
         public IOrderManager OrderManager => throw new NotImplementedException();
 
         public Task<string> ConnectAsync()
         {
-            throw new NotImplementedException();
+            if (_isConnected)
+                throw new ErrorMessage("Already connected");
+
+            _isConnected = true;
+            return Task.FromResult<string>("FAKEACCOUNT123");
         }
 
         public Task DisconnectAsync()
         {
-            throw new NotImplementedException();
+            _isConnected = false;
+            return Task.CompletedTask;
         }
 
         public Task<Account> GetAccountAsync(string accountCode)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(_fakeAccount);
         }
     }
 }
