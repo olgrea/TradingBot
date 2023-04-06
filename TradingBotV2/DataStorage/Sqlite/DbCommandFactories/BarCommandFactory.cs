4a73fe78ebee5898d664617ffe795112c69df48e
olgrea <olgrea@gmail.com>
2023-04-06 19:49:34 -0400

    migrated sqlite db classes to new project

---
+++ TradingBotV2/DataStorage/Sqlite/DbCommandFactories/BarCommandFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using TradingBotV2.Broker.MarketData;
+using TradingBotV2.DataStorage.Sqlite.DbCommands;
+
+namespace TradingBotV2.DataStorage.Sqlite.DbCommandFactories
+{
+    public class BarCommandFactory : DbCommandFactory<Bar>
+    {
+        BarLength _barLength;
+
+        public BarCommandFactory(BarLength barLength, SqliteConnection connection = null) : base(connection)
+        {
+            _barLength = barLength;
+        }
+
+        public override DbCommand<bool> CreateExistsCommand(string symbol, DateTime date)
+        {
+            return CreateExistsCommand(symbol, date, MarketDataUtils.MarketDayTimeRange);
+        }
+
+        public override DbCommand<bool> CreateExistsCommand(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange)
+        {
+            return new BarExistsCommand(symbol, date, timeRange, _barLength, _connection);
+        }
+
+        public override DbCommand<IEnumerable<Bar>> CreateSelectCommand(string symbol, DateTime date)
+        {
+            return new SelectBarsCommand(symbol, date, MarketDataUtils.MarketDayTimeRange, _barLength, _connection);
+        }
+
+        public override DbCommand<IEnumerable<Bar>> CreateSelectCommand(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange)
+        {
+            return new SelectBarsCommand(symbol, date, timeRange, _barLength, _connection);
+        }
+
+        public override DbCommand<bool> CreateInsertCommand(string symbol, IEnumerable<Bar> dataCollection)
+        {
+            return new InsertBarsCommand(symbol, dataCollection, _connection);
+        }
+    }
+}
---
+++ TradingBotV2/DataStorage/Sqlite/DbCommandFactories/LastCommandFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using TradingBotV2.Broker.MarketData;
+using TradingBotV2.DataStorage.Sqlite.DbCommands;
+
+namespace TradingBotV2.DataStorage.Sqlite.DbCommandFactories
+{
+    public class LastCommandFactory : DbCommandFactory<Last>
+    {
+        public LastCommandFactory(SqliteConnection connection = null) : base(connection)
+        {
+        }
+
+        public override DbCommand<bool> CreateExistsCommand(string symbol, DateTime date)
+        {
+            return new LastExistsCommand(symbol, date, MarketDataUtils.MarketDayTimeRange, _connection);
+        }
+
+        public override DbCommand<bool> CreateExistsCommand(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange)
+        {
+            return new LastExistsCommand(symbol, date, timeRange, _connection);
+        }
+
+        public override DbCommand<IEnumerable<Last>> CreateSelectCommand(string symbol, DateTime date)
+        {
+            return new SelectLastsCommand(symbol, date, MarketDataUtils.MarketDayTimeRange, _connection);
+        }
+
+        public override DbCommand<IEnumerable<Last>> CreateSelectCommand(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange)
+        {
+            return new SelectLastsCommand(symbol, date, timeRange, _connection);
+        }
+
+        public override DbCommand<bool> CreateInsertCommand(string symbol, IEnumerable<Last> dataCollection)
+        {
+            return new InsertLastsCommand(symbol, dataCollection, _connection);
+        }
+    }
+}
