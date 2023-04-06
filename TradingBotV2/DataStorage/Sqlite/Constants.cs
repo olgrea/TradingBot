33bc3809a983f91dbbf1f556d4aa621180eae0dd
olgrea <olgrea@gmail.com>
2023-04-06 19:49:34 -0400

    migrated sqlite db classes to new project

---
+++ TradingBotV2/DataStorage/Sqlite/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TradingBotV2.DataStorage.Sqlite
+{
+    public static class Constants
+    {
+        public const string DbPath = @"C:\tradingbot\db\historicaldata.sqlite3";
+        public const string DbConnectionString = "Data Source=" + DbPath;
+    }
+}
---
+++ TradingBotV2/DataStorage/Sqlite/DbCommandFactories/DbCommandFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using TradingBotV2.Broker.MarketData;
+using TradingBotV2.DataStorage.Sqlite;
+using TradingBotV2.DataStorage.Sqlite.DbCommands;
+
+namespace TradingBotV2.DataStorage.Sqlite.DbCommandFactories
+{
+    public abstract class DbCommandFactory<TMarketData> where TMarketData : IMarketData, new()
+    {
+        protected SqliteConnection _connection;
+
+        protected DbCommandFactory(SqliteConnection connection = null)
+        {
+            if (connection == null)
+            {
+                connection = new SqliteConnection(Constants.DbConnectionString);
+                connection.Open();
+            }
+
+            _connection = connection;
+        }
+
+        ~DbCommandFactory()
+        {
+            _connection?.Close();
+            _connection?.Dispose();
+        }
+
+        public abstract DbCommand<bool> CreateExistsCommand(string symbol, DateTime date);
+        public abstract DbCommand<bool> CreateExistsCommand(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange);
+        public abstract DbCommand<IEnumerable<TMarketData>> CreateSelectCommand(string symbol, DateTime date);
+        public abstract DbCommand<IEnumerable<TMarketData>> CreateSelectCommand(string symbol, DateTime date, (TimeSpan, TimeSpan) timeRange);
+        public abstract DbCommand<bool> CreateInsertCommand(string symbol, IEnumerable<TMarketData> dataCollection);
+    }
+}
