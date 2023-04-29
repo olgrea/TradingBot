59260e33f68b32ac04a31768fcf4ee702beef312
olgrea <olgrea@gmail.com>
2023-04-29 12:15:02 -0400

    IBHistoricalDataProvider rework. (UNTESTED, need to wait for pacing violations...)

--- TradingBotV2/Broker/MarketData/Providers/IHistoricalDataProvider.cs
+++ TradingBotV2/Broker/MarketData/Providers/IHistoricalDataProvider.cs
@@ -2,9 +2,11 @@
 {
     public interface IHistoricalDataProvider
     {
-        public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateTime date) where TData : IMarketData, new();
+        public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateOnly date) where TData : IMarketData, new();
+        public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateTime dateTime) where TData : IMarketData, new();
         public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateTime from, DateTime to) where TData : IMarketData, new();
-        public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateTime date, CancellationToken token) where TData : IMarketData, new();
+        public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateOnly date, CancellationToken token) where TData : IMarketData, new();
+        public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateTime dateTime, CancellationToken token) where TData : IMarketData, new();
         public Task<IEnumerable<IMarketData>> GetHistoricalDataAsync<TData>(string ticker, DateTime from, DateTime to, CancellationToken token) where TData : IMarketData, new();
     }
 }
